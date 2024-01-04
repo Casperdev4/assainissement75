185231c338d8ed8bcb10973d7139f9f88fe661ee
Casperdev4 <allam.bilal91@gmail.com>
2024-01-04 20:37:16 +0100

    gg

--- traitement.php
+++ traitement.php
@@ -28,8 +28,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'contact.webprime@gmail.com';                     //SMTP username
-    $mail->Password   = 'ddivwnbnyyhrfbbn';                               //SMTP password
+    $mail->Username   = 'contact@webprime.fr';                     //SMTP username
+    $mail->Password   = 'Allamalyjass912!';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
