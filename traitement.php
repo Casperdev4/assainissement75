35b934ad592672007086dee38aa819b67274b242
Casperdev4 <allam.bilal91@gmail.com>
2024-01-04 19:20:53 +0100

    gg

--- traitement.php
+++ traitement.php
@@ -39,7 +39,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'Formulaire de contact';
     $mail->Body    = $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
